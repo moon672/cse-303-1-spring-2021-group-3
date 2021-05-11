1173fa038b1bdf4bf7332a2d1c22a71b290799b0
Niloy1178 <tniloy85@gmail.com>
2021-05-11 11:07:10 +0600

    FrontEnd Final

---
+++ Report/FrontEnd/spms/App/new_template_bottom.php
@@ -0,0 +1,12 @@
+<!-- Main Contents of Current Page End Point -->
+					</div>
+					<!-- Main Content Part Excluding Top Menu, Side Menu and Footer End Point -->
+				</div>
+				<!-- Main Content Part of Current Page including Common Things End Point-->
+				<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        </div>
+		<!--All parts below Top Menu End point -->
+	</div>
+	<script type="text/javascript" src="./assets/scripts/main.js"></script>
+</body>
+</html>
